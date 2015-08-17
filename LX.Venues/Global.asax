f5348d49f0649776bc05f25b4c51a408142b71e2
rizwanmahfooz <rizwanmahfooz.pu@gmail.com>
2015-08-17 23:31:48 +0500

    c1

---
+++ LX.Venues/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LX.Venues.MvcApplication" Language="C#" %>
---
+++ LX.Venues/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LX.Venues.Startup))]
+namespace LX.Venues
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
